94c305169d37d960c77841c43baa1d77b8e318fe
TranAiQuoc <tranquoc12x@gmail.com>
2025-12-16 21:33:58 +0700

    first commit

---
+++ backend/api.php
@@ -0,0 +1,23 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json; charset=UTF-8");
+include 'db.php';
+
+try {
+    $sql = "SELECT * FROM users";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $data = $stmt->fetchAll();
+    if (count($data) > 0) {
+        echo json_encode($data);
+    } else {
+        echo json_encode([
+            ["id" => 1, "name" => "Demo User PDO", "email" => "pdo@test.com"],
+            ["id" => 2, "name" => "Nguyen Van A", "email" => "nva@test.com"]
+        ]);
+    }
+} catch (Exception $e) {
+    http_response_code(500);
+    echo json_encode(["error" => "Lỗi truy vấn: " . $e->getMessage()]);
+}
---
+++ backend/db.php
@@ -0,0 +1,21 @@
+<?php
+$host = "sql302.infinityfree.com";
+$dbname = "if0_40245835_phpproject";
+$username = "if0_40245835";
+$password = "H8pHYNWlysy";
+
+try {
+
+    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+
+    $conn = new PDO($dsn, $username, $password, $options);
+} catch (PDOException $e) {
+    header("Content-Type: application/json; charset=UTF-8");
+    echo json_encode(["error" => "Lỗi kết nối Database: " . $e->getMessage()]);
+    exit();
+}
